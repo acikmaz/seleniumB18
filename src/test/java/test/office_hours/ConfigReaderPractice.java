42b533a9a7b4401f9f6b9b2ec37b374845325296
acikmaz <acikmaz19@gmail.com>
2020-07-01 17:52:15 -0500

    office hours

---
+++ src/test/java/test/office_hours/ConfigReaderPractice.java
@@ -0,0 +1,72 @@
+package test.office_hours;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import utilities.WebDriverFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReaderPractice {
+
+    private static Properties properties;
+    WebDriver driver;
+
+    static {
+        try(FileInputStream fileInputStream = new FileInputStream("src/test/resources/config.properties");){
+            properties = new Properties();
+            properties.load(fileInputStream);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public  static  String getProperty(String key){
+        return properties.getProperty(key);
+    }
+
+    public static void main(String[] args) {
+        /**
+         * to go into method/class implementation click and hold control/command and click
+         * @param args
+         */
+        String browser = properties.getProperty("browser");
+        System.out.println(browser);
+        // you will get null if there is no such a property
+        System.out.println(properties.getProperty("url"));
+
+
+    }
+
+    @Test
+    public void loginTest(){
+        String browser = getProperty("browser");
+        String URL = getProperty("url");
+        String username = getProperty("username");
+        String password = getProperty("password");
+
+        driver = WebDriverFactory.getDriver(browser);
+        driver.get(URL);
+        System.out.println(driver.getTitle());
+
+        //login
+        driver.findElement(By.id("prependedInput")).sendKeys(username);
+        driver.findElement(By.id("prependedInput2")).sendKeys(password, Keys.ENTER);
+
+        String expected = "Dashboard";
+        String actual = driver.getTitle();
+        Assert.assertEquals(actual, expected, "Page title is not correct");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
